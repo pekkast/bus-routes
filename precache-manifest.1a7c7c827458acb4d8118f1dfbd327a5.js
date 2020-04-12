5708ef27c228d62de4ece78de2a48c9e44d84816
Pekka Turtola <pekka.turtola@optima.fi>
2020-04-12 23:25:38 +0300

    Updates

---
+++ precache-manifest.1a7c7c827458acb4d8118f1dfbd327a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480ab50ae072c044d0e4065e587d7a73",
+    "url": "/bus-routes/index.html"
+  },
+  {
+    "revision": "5171f09747adb75ad7df",
+    "url": "/bus-routes/static/js/2.c6f048e8.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/bus-routes/static/js/2.c6f048e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd299641b4ba44045d9",
+    "url": "/bus-routes/static/js/main.3e47ab5a.chunk.js"
+  },
+  {
+    "revision": "2958e3c2b8c860098cab",
+    "url": "/bus-routes/static/js/runtime-main.c87ce563.js"
+  }
+]);
